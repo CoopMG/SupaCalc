0ce255acc5ef37e0c4fd91a209f63bf7b9cc6bef
Cooper Grant <coopermgrant@gmail.com>
2024-06-25 11:36:14 +0800

    implemented output field

--- supacalc/src/App.jsx
+++ supacalc/src/App.jsx
@@ -1,11 +1,13 @@
 //CODE FOR WHAT WILL RENDER
 import './App.css';
 import { HeaderComponent } from './Components/Header/Header';
+import { OutputFieldComponent } from './Components/OutputFieldComponent/OutputFieldComponent';
 
 function App() {
   return (
     <div className = "screen">
       <HeaderComponent/>
+      <OutputFieldComponent/>
     </div>
   );
 }
---
+++ supacalc/src/Components/OutputFieldComponent/OutputFieldComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './OutputFieldComponent.css'
+
+export const OutputFieldComponent = () => {
+
+    return(
+    <div className = "OutputField">
+        <p className='OutputText'>7</p>
+    </div>
+    )
+}
